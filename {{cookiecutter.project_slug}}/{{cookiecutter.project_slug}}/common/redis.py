be3031b5f19047399d7f355542d3dceaa92eb7fa
Jianing Yang <jianingy.yang@gmail.com>
2019-04-23 10:38:32 +0800

    add redis support

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/redis.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from gevent import socket as gsocket
+import redis
+import redis.connection
+
+from .exceptions import RedisFactoryError
+
+redis.connection.socket = gsocket
+
+
+class RedisFactory:
+    _pools = {}
+
+    def __init__(self):
+        raise RuntimeError('cannot initialize an instance of this class')
+
+    @classmethod
+    def connect(cls, uri, name='default'):
+        if name not in cls._pools:
+            cls._pools[name] = redis.ConnectionPool.from_url(uri)
+        return redis.StrictRedis(connection_pool=cls._pools[name])
+
+    @classmethod
+    def create(cls, name='default'):
+        if name not in cls._pools:
+            raise RedisFactoryError(reason='redis connection not connected')
+        return redis.StrictRedis(connection_pool=cls._pools[name])
+
+    @classmethod
+    def get_connection_pool(cls, name='default'):
+        if name not in cls._pools:
+            raise RedisFactoryError(reason='redis connection not connected')
+        return cls._pools[name]
